ee832bd40fabeaad301e4c0a5c7075f350cf3df6
shinyahirai <908.shinya@gmail.com>
2015-10-17 01:11:30 +0800

    day5

--- twitter_bbs/join/check.php
+++ twitter_bbs/join/check.php
@@ -1,3 +1,18 @@
+<?php
+    session_start();
+
+    // もしindex.phpを経由せず直接check.phpにアクセスがあった場合の処理
+    if (!isset($_SESSION["join"])) {
+        header("Location: index.php");
+        exit();
+    }
+
+    // htmlspecialcharsのショートカット
+    function h($value) {
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -11,18 +26,27 @@
     <div>
       <p>ニックネーム</p>
       <!-- 入力されたニックネームの値 -->
+      <?php echo h($_SESSION["join"]["nickname"]); ?>
     </div>
     <div>
       <p>メールアドレス</p>
       <!-- 入力されたメールアドレスの値 -->
+      <?php echo h($_SESSION["join"]["email"]); ?>
     </div>
     <div>
       <p>パスワード</p>
       <!-- 入力されたパスワードの値 -->
+      【表示されません】
     </div>
     <div>
       <p>プロフィール画像</p>
       <!-- 選択された画像 -->
+      <?php
+          echo sprintf('<img src="../member_picture/%s" width="100" height="100">',
+              $_SESSION["join"]["image"]
+          );
+      ?>
+      
     </div>
     
     <div>
--- twitter_bbs/join/index.php
+++ twitter_bbs/join/index.php
@@ -12,7 +12,8 @@
     // !の機能
     //// 関数等の前に付けてif文の条件にいれるとtrueもしくはfalseを逆に変換します
     if (!empty($_POST)) {
-        // エラー項目の確認
+
+        // エラー項目の確認 //
         if ($_POST["nickname"] == '') {
             $error["nickname"] = 'blank';
         }
@@ -31,9 +32,27 @@
             $error["password"] = 'blank';
         }
 
+        $fileName = $_FILES["image"]["name"];
+        if (!empty($fileName)) {
+            // substr関数
+            // 第一引数に入れた文字列から第二引数に入れた文字数から始まる文字を取得
+            $ext = substr($fileName, -3);
+            if ($ext != 'jpg' && $ext != 'gif') {
+                $error["image"] = "type";
+            }
+        }
+        ////////////
 
+
+        // エラーがなければ次のページにいくための処理
         if (empty($error)) {
+
+            // 画像アップロード
+            $image = date('YmdHis') . $_FILES["image"]["name"];
+            move_uploaded_file($_FILES["image"]["tmp_name"], '../member_picture/' . $image);
+
             $_SESSION["join"] = $_POST;
+            $_SESSION["join"]["image"] = $image;
 
             header('Location: check.php');
             exit();
@@ -63,7 +82,7 @@
       <?php
           if (isset($_POST["nickname"])) {
               echo sprintf('<input type="text" name="nickname" value="%s">',
-                h($_POST["nickname"])
+                  h($_POST["nickname"])
               );
           } else {
               echo '<input type="text" name="nickname">';
@@ -79,16 +98,55 @@
 
     <div>
       <label for="">メールアドレス</label>
-      <input type="text" name="email">
+      <?php
+          if (isset($_POST["email"])) {
+              echo sprintf('<input type="text" name="email" value="%s">',
+                  h($_POST["email"])
+              );
+          } else {
+              echo '<input type="text" name="email">';
+          }
+      ?>
+
+      <?php if (isset($error["email"])): ?>
+          <?php if ($error["email"] == 'blank'): ?>
+              <p class="error">* メールアドレスを入力してください。</p>
+          <?php endif; ?>    
+      <?php endif; ?>
     </div>
     
     <div>
       <label for="">パスワード</label>
-      <input type="password" name="password">
+      <?php
+          if (isset($_POST["password"])) {
+              echo sprintf('<input type="text" name="password" value="%s">',
+                  h($_POST["password"])
+              );
+          } else {
+              echo '<input type="password" name="password">';
+          }
+      ?>
+
+      <?php if (isset($error["password"])): ?>
+          <?php if ($error["password"] == 'blank'): ?>
+              <p class="error">* パスワードを入力してください。</p>
+          <?php endif; ?>
+          <?php if ($error["password"] == 'length'): ?>
+              <p class="error">* パスワードは4文字以上で入力してください。</p>
+          <?php endif; ?>
+      <?php endif; ?>
     </div>
 
     <div>
       <label for="">プロフィール画像</label>
+      <?php if (isset($error["image"])): ?>
+          <?php if ($error["image"] == 'type'): ?>
+              <p class="error">* 画像形式はjpgもしくはgifを指定してください。</p>
+          <?php endif; ?>
+          <?php if (!empty($error)): ?>
+              <p class="error">* 画像を改めて指定してください。</p>
+          <?php endif; ?>
+      <?php endif; ?>
       <input type="file" name="image">
     </div>
 
